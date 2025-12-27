07641b419aace8d6506bc17d70977a03d69ab39a
MarinaHurricane <maryishch88@gmail.com>
2025-12-27 14:45:53 +0000

    minor fix

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,6 +1,6 @@
 const name: string = "Alice";
 const age: number = 30;
-const isOnline: Boolean = true;
+const isOnline: boolean = true;
 
 console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
